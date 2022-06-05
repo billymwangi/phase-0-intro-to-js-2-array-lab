b58f6ebd33ffa15012038a0193cc43f4eabb2a59
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-05 16:08:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("charlie")
+}
+function destructivelyPrependCat() {
+    cats.unshift("jamie")
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat() {
+
+}
+function prependCat() {
+
+}
+function removeLastCat() {
+
+}
+function removeFirstCat() {
+
+}
